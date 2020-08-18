2b212c642e05a90d5ea2f1a344eca224efbf8c06
Tim Ackroyd <timothy.ackroyd@gmail.com>
2020-08-18 21:39:22 +1200

    Add tailwind
    - also removed CRA boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
