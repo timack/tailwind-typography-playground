9e14158e976d18c35347141bda54c1a1fae4d911
Tim Ackroyd <timothy.ackroyd@gmail.com>
2020-08-22 09:01:51 +1200

    Tidy up styles on control panel

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import DemoContent from "./DemoContent";
 const axios = require("axios").default;
 
 export default class App extends Component {
-
   state = {
     proseSize: "",
     disableMaxWidth: false,
     fonts: [],
   };
-
-
 
   componentDidMount() {
     // Load font list from google
@@ -57,58 +54,72 @@ export default class App extends Component {
     ];
 
     return (
-      <div className="grid grid-cols-12 gap-12 p-12 h-screen bg-gradient-to-r from-green-300 via-yellow-200 to-red-400">
-        <div className="bg-white col-span-3 rounded-lg shadow-xl p-6">
-          <h3 className="mb-3 uppercase text-gray-700 font-semibold text-base tracking-widest">
-            Configure
-          </h3>
+      <div className="grid grid-cols-12 gap-12 p-12 h-screen bg-gray-200">
+        <div className="bg-white col-span-3 rounded-lg shadow p-6">
+          <h3 className="mb-3 font-semibold text-lg">Configure</h3>
 
-          <h4 className="uppercase text-gray-600 font-semibold text-sm tracking-widest">
-            Size
-          </h4>
-          <div className="flex items-stretch bg-gray-600">
-            {sizeOptions.map((option, idx) => (
-              <button
-                key={idx}
-                className={`flex-1 text-xs uppercase py-2 px-4 text-gray-200 font-bold border-white border-r last:border-r-0 hover:bg-gray-700 ${
-                  this.state.proseSize === option.class
-                    ? "bg-gray-800"
-                    : "bg-gray-600"
-                }`}
-                onClick={(e) => this.handleSizeClick(option.class, e)}
-              >
-                {option.display}
-              </button>
-            ))}
-          </div>
-          <h4 className="uppercase text-gray-600 font-semibold text-sm tracking-widest">
-            Font
-          </h4>
-          <div>
-            <select name="" id="">
-              <option value="">Default</option>
-              {this.state.fonts.map((font, idx) => (
-                <option key={idx} value="">
-                  {font.family}
-                </option>
+          <div className="py-2">
+            <h4 className="font-semibold text-sm pb-1">Size</h4>
+            <div className={`grid grid-cols-${sizeOptions.length} gap-2`}>
+              {sizeOptions.map((option, idx) => (
+                <button
+                  key={idx}
+                  className={`border border-gray-500 text-xs uppercase py-2 px-4 hover:bg-gray-700 hover:text-gray-200 ${
+                    this.state.proseSize === option.class
+                      ? "bg-gray-800 text-gray-200"
+                      : "bg-white text-gray-700"
+                  }`}
+                  onClick={(e) => this.handleSizeClick(option.class, e)}
+                >
+                  {option.display}
+                </button>
               ))}
-            </select>
+            </div>
           </div>
-          <h4 className="uppercase text-gray-600 font-semibold text-sm tracking-widest">
-            Remove Max-Width
-          </h4>
-          <div>
-            <label htmlFor="">Disable Max Width</label>
-            <input
-              type="checkbox"
-              name=""
-              id=""
-              checked={this.state.disableMaxWidth}
-              onChange={this.handleMaxWidthClick}
-            />
+
+          <div className="py-2">
+            <h4 className="font-semibold text-sm pb-1">
+              Font
+            </h4>
+            <div className="relative">
+              <select
+                name="fontSelection"
+                id="fontSelection"
+                className="w-full border border-gray-500 p-3 appearance-none"
+              >
+                <option value="">Default</option>
+                {this.state.fonts.map((font, idx) => (
+                  <option key={idx} value="">
+                    {font.family}
+                  </option>
+                ))}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 pr-3 right-0 top-0 flex items-center">
+                <svg viewBox="0 0 20 20" fill="currentColor" className="fill-current w-6 h-6"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2">
+            <h4 className="font-semibold text-sm pb-1">
+              Remove Max-Width
+            </h4>
+            <div className="flex items-baseline">
+              <input
+                type="checkbox"
+                name="disableMaxWidth"
+                id="disableMaxWidth"
+                checked={this.state.disableMaxWidth}
+                onChange={this.handleMaxWidthClick}
+                className="mr-3"
+              />
+              <label htmlFor="disableMaxWidth">Disable Max Width</label>
+            </div>
           </div>
         </div>
-        <div className="bg-white col-span-9 rounded-lg shadow-xl py-12 px-20 overflow-scroll">
+
+        {/* Example page */}
+        <div className="bg-white col-span-9 rounded-lg shadow py-12 px-20 overflow-scroll">
           <article
             className={`mx-auto prose ${this.state.proseSize} ${
               this.state.disableMaxWidth ? "max-w-none" : ""
@@ -117,6 +128,7 @@ export default class App extends Component {
             <DemoContent />
           </article>
         </div>
+
       </div>
     );
   }
